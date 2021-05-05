f44628ce313d7fe426a7426a52b0d8380eb98ded
Ishaaq-dev <i.iqbalwork@gmail.com>
2021-05-05 19:28:40 +0100

    Adds working TrieDataStructure

---
+++ src/trie/Trie.java
@@ -0,0 +1,102 @@
+package trie;
+
+import java.util.ArrayList;
+
+public class Trie {
+	
+	static final int SIZE = 26;
+	
+	static class TrieNode {
+		
+	    TrieNode[] Child = new TrieNode[SIZE];
+	   
+	    private boolean isWord;
+	      
+	    // Constructor
+	    public TrieNode() {
+	        isWord = false;
+	        for (int i =0 ; i< SIZE ; i++)
+	                Child[i] = null;
+	    }
+	    
+	    public void setIsWord(boolean isWord) {
+	    	this.isWord = isWord;
+	    }
+	    
+	    public boolean getIsWord() {
+	    	return isWord;
+	    }
+	}
+    
+    static void insert(TrieNode root, String Key)
+    {
+        int n = Key.length();
+        TrieNode pChild = root;
+       
+        for (int i=0; i<n; i++)
+        {
+            int index = Key.charAt(i) - 'a';
+       
+            if (pChild.Child[index] == null)
+                pChild.Child[index] = new TrieNode();
+       
+            pChild = pChild.Child[index];
+        }
+       
+        pChild.setIsWord(true);
+    }
+       
+    static void searchWord(TrieNode root, boolean Hash[], String str, ArrayList<String> usuableWords) {
+        if (root.getIsWord() == true)
+            usuableWords.add(str);
+       
+        for (int K =0; K < SIZE; K++)
+        {
+            if (Hash[K] == true && root.Child[K] != null )
+            {
+                char c = (char) (K + 'a');
+       
+                searchWord(root.Child[K], Hash, str + c, usuableWords);
+            }
+        }
+    }
+       
+    static void PrintAllWords(char Arr[], TrieNode root, 
+                                              int n, ArrayList<String> usuableWords)
+    {
+        boolean[] Hash = new boolean[SIZE];
+       
+        for (int i = 0 ; i < n; i++)
+            Hash[Arr[i] - 'a'] = true;
+       
+        TrieNode pChild = root ;
+       
+        String str = "";
+       
+        for (int i = 0 ; i < SIZE ; i++)
+        {
+            if (Hash[i] == true && pChild.Child[i] != null )
+            {
+                str = str+(char)(i + 'a');
+                searchWord(pChild.Child[i], Hash, str, usuableWords);
+                str = "";
+            }
+        }
+    }
+    
+    public ArrayList<String> getUsuableWords(String[] dictionary, char uniqueLetters[]) {
+    	ArrayList<String> usuableWords = new ArrayList<String>();
+    	
+    	TrieNode node = new TrieNode();
+        
+        int n = dictionary.length;
+        for (int i=0; i<n; i++)
+            insert(node, dictionary[i]);
+       
+        int N = uniqueLetters.length;
+       
+        PrintAllWords(uniqueLetters, node, N, usuableWords);
+    	
+    	return usuableWords;
+    }
+}
