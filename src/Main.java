7d0ed62bf759e9a577d758b9710ab05a65a46efc
Ishaaq-dev <i.iqbalwork@gmail.com>
2021-05-09 21:21:22 +0100

    adds functionality to main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-//import java.io.File;
-//import java.io.FileWriter;
-//import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.List;
+import java.util.Scanner;
 
 import dataManagement.DataHandler;
 import dataStructures.Trie_Shaq;
@@ -13,12 +9,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		DataHandler dh = new DataHandler();
-		Trie_Shaq trie = dh.generateTrieForSquare(4, "eeeeddoonnnsssrv");
 		
-		System.out.println("done");
+		boolean check = false;
+		String letters = "";
+		while (!check) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter pool of letters");
+			letters = scanner.nextLine().toLowerCase();
+			check = validateInput(letters);
+		}
+		
+		int sizeOfGrid = (int) Math.sqrt(letters.length());
+		
+		Trie_Shaq dictionary_trie = dh.generateTrieForSquare(sizeOfGrid, letters);
+		List<String> dictionary = null;
+	}
+	
+	private static boolean validateInput(String input) {
+		boolean check = false;
+		if (Math.sqrt(input.length()) % 1 == 0) {
+			if(!input.matches("^[a-z]")) check = true;
+			else System.out.println("Input must only contain letters");
+		} else System.out.println("Input length can not be square rooted");
+		
+		return check;
 	}
 
 }
-
-//eeeeddoonnnsssrv
-//aaaeeeefhhmoonssrrrrttttw
--- src/dataManagement/DataHandler.java
+++ src/dataManagement/DataHandler.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import dataStructures.Trie;
@@ -78,7 +79,15 @@ public class DataHandler {
 	}
 	
 	public Trie_Shaq generateTrieForSquare(int sizeOfGrid, String letters) {
+		Map<Character, HashSet<String>> wordMapForSquare = getWordMapForSquare(sizeOfGrid, letters);
+		
+		return Trie.getUsuableWords(wordMapForSquare); 
+	}
+	
+	
+	public Map<Character, HashSet<String>> getWordMapForSquare(int sizeOfGrid, String letters) {
 		Map<Character, HashSet<String>> wordMapForSquare = new HashMap<Character, HashSet<String>>();
+	
 		double squareRootLengthOfLetters = Math.sqrt(letters.length());
 		if (sizeOfGrid != squareRootLengthOfLetters) {
 			System.out.println("Size of grid specified: " + sizeOfGrid + "\n"
@@ -93,6 +102,6 @@ public class DataHandler {
 			wordMapForSquare.put(letter.charAt(0), getHashSetOfWordsByLength(letter.charAt(0), sizeOfGrid));
 		});
 		
-		return Trie.getUsuableWords(wordMapForSquare); 
+		return wordMapForSquare;
 	}
 }
